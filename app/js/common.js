be8f36a277b6d56d60f0ceec9d2ed19f11e762db
Viktoria <zaharova181114@gmail.com>
2021-08-27 17:44:20 +0300

    add page

--- app/js/common.js
+++ app/js/common.js
@@ -9,3 +9,18 @@ $('.btn-close').on('click', function () {
 $('[name="phone"]').mask('+7 (999) 999-99-99');
 
 new WOW().init();
+
+$('.down').on("click", function () {
+    let $input = $(this).parent().find('input');
+    let count = parseInt($input.val()) - 1;
+    count = count < 1 ? 1 : count;
+    $input.val(count);
+    $input.change();
+    return false;
+});
+$('.up').on("click",function () {
+    let $input = $(this).parent().find('input');
+    $input.val(parseInt($input.val()) + 1);
+    $input.change();
+    return false;
+});
